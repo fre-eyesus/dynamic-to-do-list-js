0f5a307edc37a3d887e96aac4f8aef1787ef7af0
Fireweld Tesfaye <freyesus12@gmail.com>
2025-01-26 22:29:23 +0300

    write the script for te TO-DO app

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        if (taskText === "") {
+            alert('Please enter a task');
+            return;
+        } else {
+            const task = document.createElement('li');
+            task.textContent = taskText;
+            const remove = document.createElement('button');
+            remove.textContent = "Remove";
+            remove.className = "remove-btn";
+            remove.addEventListener('click', function() {
+                taskList.removeChild(task);
+            });
+            task.appendChild(remove);
+            taskList.appendChild(task);
+            taskInput.value = "";
+        }
+    }
+
+    addButton.addEventListener('click', function() {
+        addTask();
+    });
+
+    taskInput.addEventListener('keypress', function(e) {
+        if (e.key === "Enter") {
+            addTask();
+        }
+    });
+});
